556640a8cba5711616d8d89dc7e82ef82031b810
Ashley Mathew <amathew195@gmail.com>
2022-10-11 20:06:05 -0500

    finished wordfinder

--- wordfinder.py
+++ wordfinder.py
@@ -6,24 +6,32 @@ class WordFinder:
 
     def __init__(self, path):
         """Create instance of word finder from path of a text file."""
-        self.path = path
-        self.words = self.read_file()
+        file = open(path)
+        # self.path = path
+        self.words = self.get_words_list(file)
+        self.report_words_length()
 
     def __repr__(self):
         """Review representation of word finder instance."""
         return f"<WordFinder path={self.path} words={self.words[:3]}>"
 
-    def read_file(self):
+    def get_words_list(self, file):
         """Reads the file, prints out the number of words in file and
         returns a list of words."""
-        file = open(self.path, "r")
-        words = file.read().splitlines()
-        print(f"{len(words)} words read")
-        return words
+        # file = open(self.path, "r")
+        # words = file.read().splitlines()
+        # return words
+        return [line.strip() for line in file]
+
+    def report_words_length(self):
+        print(f"{len(self.words)} words read")
 
     def random(self):
         """Returns a random word from the list of words."""
         return choice(self.words)
 
 
-    # Subclass, split things differently (some refactoring)
+class SpecialWordFinder(WordFinder):
+    def get_words_list(self, file):
+        return [word for word in super().get_words_list(file)
+                if word != "" and not word.startswith("#")]
